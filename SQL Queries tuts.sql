c0b911bbde57735275898d6b725b4d9c4b31f6b1
Peter Mathenge <7058911+PMathenge@users.noreply.github.com>
2022-03-02 03:22:45 +0300

    Update SQL Queries tuts.sql
    Review part 7

--- SQL Queries tuts.sql
+++ SQL Queries tuts.sql
@@ -5,11 +5,17 @@ CONNECTING TO SQL SERVER
 --SSMS is a client tool and not the server by itself
 --Developer Machine(client) connects to SQL Server using SSMS
 
+-- .MDF file - Data File(Contains actual Data)
+-- .LDF - Transaction Log File(used to recover the database)
+
 --Change db name
 Alter Database Sample2 Modify Name = Sample3
 sp_renameDB 'Sample3','Sample4'
 
 --drop a database
+--You cannot drop a database if users are connected change database in single use mode and drop
+Alter Database DatabaseName Set SINGLE_USER With Rollback Immediate
+
 --you can't drop db if it is currently in Use
 Drop database Sample3
 
@@ -28,11 +34,13 @@ Foreign key (GenderID) references tblGender (ID)
 --System databases cannot be dropped.
 Alter database DatabaseName SET SINGLE_USER With Rollback Immediate
 
+--Altering an existing column to add a default contraint.
 ALTER TABLE(TABLE_NAME)
 ADD CONSTRAINT(CONSTRAINT_NAME)
 DEFAULT (DEFAULT_VALUE) FOR (EXISTING_COLUMN_NAME)
 
 --Adding a new column, with default value, to an existing table
+--Dafault constraint is only considered if you dont suppy a value
 ALTER TABLE(TABLE_NAME)
 ADD(COLUMN_NAME)(DATA_TYPE)(NULL|NOT NULL)
 CONSTRAINT(CONSTANT_NAME) DEFAULT (DEFAULT_VALUE)
@@ -40,6 +48,34 @@ CONSTRAINT(CONSTANT_NAME) DEFAULT (DEFAULT_VALUE)
 --Dropping a constraint
 ALTER TABLE(TABLE_NAME)
 DROP CONSTRAINT(CONSTRAINT_NAME)
+
+--cascading referential integrity constraint allow to define the actions MSSQL Server should take when a users attempts 
+--to delete or update a key to which an existing foreign key exists
+/*OPTIONS
+1. No Action
+This is the default behaviour. No action specifies that if an attempt is made to delete or update a row with a key referenced 
+by foreign keys in existing rows in other tables, an error is raised and the delete or update is rolled back
+2. Cascade
+Specifies that if an attempt is made to delete or update a row with a key referenced by foreign keys in existing rows or
+tables, all rows containing those foreign keys are also deleted or updated
+3. Set NULL
+Specifies that if an attempt is made to delete or update a row with a key referenced by foreign keys in existing rows in other tables
+all rows containing those foreign keys are set to NULL
+4. SET DEFAULT 
+Specifies that if an attempt is made to delete or update a row with a key referenced by foreign keys in existing rows in other tables
+all rows containing those foreign keys are set to DEFAULT*/
+
+/*CHECK CONSTRAINT
+-Used to limit the range of the values, that can be entered for a column
+
+Genral formula
+ALTER TABLE {TABLE_NAME}
+ADD CONSTRAINT {CONSTRAINT_NAME} CHECK (BOOLEAN_EXPRESSION)
+
+DROP CONSTRAINT
+ALTER TABLE TBLPERSON
+DROP CONSTRAINT CK_TBLPERSON_AGE
+*/
 
 --Inner join returns only the matching rows between both the tables.
 --non matching rows are eliminated
